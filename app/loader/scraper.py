a2b30107afa50674733306fd6b31be1fdef81d50
Tommaso Tassi <tommaso.tassi@oliverwyman.com>
2023-11-09 21:05:35 +0100

    Add LoveLanghe webscraper

--- app/loader/scraper.py
+++ app/loader/scraper.py
@@ -33,6 +33,8 @@ class BaseScraper:
         """Format is expected to be 'HH:MM'"""
         is_during_day = int(opening_time.split(":")[0]) <= 18
         is_during_night = int(closing_time.split(":")[0]) >= 20
+        if not is_during_day and not is_during_night:
+            raise Exception("Event is neither during day or night.")
         return is_during_day, is_during_night
 
     def is_event_in_db(self, event_url: str) -> bool:
@@ -183,8 +185,128 @@ class GuidatorinoScraper(BaseScraper):
         self.run_event_pages()
 
 
+class LovelangheScraper(BaseScraper):
+    def __init__(self, db: Session) -> None:
+        super().__init__(db=db)
+        self.identifier: str = "lovelanghe"
+        self.root_url: str = "https://langhe.net/eventi/"
+        self.page_url: str = "https://langhe.net/eventi/page/{page}/"
+        self.event_urls: list[str] = []
+
+    def run_root_page(self) -> None:
+        response = requests.get(self.root_url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        last_page = int(
+            soup.find("div", {"class": "pagination pagination--event grid__pagination"})
+            .find_all("a")[-1]["href"]
+            .split("/")[-2]
+        )
+
+        for page in range(1, last_page + 1):
+            if page > 1:
+                response = requests.get(self.page_url.format(page=page))
+                soup = BeautifulSoup(response.content, "html.parser")
+
+            events = soup.find_all("li", {"itemscope": "itemscope"})
+            for event in events:
+                url = event["href"]
+                if not self.is_event_in_db(url):
+                    self.event_urls.append(url)
+
+    @retry(stop=stop_after_attempt(3))
+    def run_event_pages(self) -> None:
+        run_event_urls = self.event_urls.copy()
+        logging.info(f"Running {len(run_event_urls)} pages")
+        for i, url in enumerate(run_event_urls):
+            logging.info(f"{i+1}/{len(run_event_urls)}")
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            city, place = [
+                s.strip()
+                for s in soup.find(
+                    "h2", {"class", "t-event__surtitle uppercase--md"}
+                ).text.split(b"\xe2\x80\x94".decode("utf-8"))
+            ]
+            is_countryside = False if city == "Torino" else True
+
+            title = soup.find("h1", {"class": "t-event__title condensed--xl"}).text
+            subtitle = soup.find("p", {"class": "t-event__subtitle serif--md"}).text
+
+            dates = soup.find_all("div", {"class": "dates__cell"})
+
+            start_date, end_date = [
+                datetime.datetime.strptime(
+                    convert_italian_month(d.find("p", {"class": "dates__full"}).text),
+                    "%d %B %Y",
+                ).date()
+                for d in dates
+            ]
+
+            opening_time = dates[0].find("p", {"class": "dates__time"}).text[5:]
+            closing_time_temp = dates[1].find("p", {"class": "dates__time"}).text
+            closing_time = (
+                "24:00"
+                if closing_time_temp == "fino a tarda notte"
+                else closing_time_temp[5:]
+            )
+            is_during_day, is_during_night = self.get_timing_flags(
+                opening_time, closing_time
+            )
+
+            contents = soup.find_all(
+                "div",
+                {
+                    "class",
+                    "content typography typography--dropcap-none base-section-col__content",
+                },
+            )
+            text = contents[0].text.strip()
+            address = contents[1].text.strip()[11:]
+
+            description = "\n".join([title, subtitle, text])
+            location = " - ".join([place, address, city])
+
+            self.output.append(
+                Event(
+                    description=description,
+                    is_vectorized=False,
+                    # metadata
+                    city=CityEnum.Torino,
+                    start_date=start_date,
+                    end_date=end_date,
+                    is_closed_mon=False,
+                    is_closed_tue=False,
+                    is_closed_wed=False,
+                    is_closed_thu=False,
+                    is_closed_fri=False,
+                    is_closed_sat=False,
+                    is_closed_sun=False,
+                    is_during_day=is_during_day,
+                    is_during_night=is_during_night,
+                    is_countryside=is_countryside,
+                    is_for_children=False,
+                    is_for_disabled=False,
+                    is_for_animals=False,
+                    # additional info
+                    name=title,
+                    location=location,
+                    url=url,
+                    price_level=None,
+                )
+            )
+            self.event_urls.remove(url)
+
+    def run(self) -> None:
+        self.run_root_page()
+        self.run_event_pages()
+
+
 SCRAPER_SUPPORTED_SOURCES = {
     "guidatorino": GuidatorinoScraper,
+    "lovelanghe": LovelangheScraper,
 }
 
 
