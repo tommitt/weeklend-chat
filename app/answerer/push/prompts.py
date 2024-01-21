fd5ca254d9fb575d10c2f683282bf0d284ee19b7
tommitt <tommytassi@hotmail.it>
2024-01-21 17:37:10 +0100

    Prompt tuning: go back to not asking for more details

--- app/answerer/push/prompts.py
+++ app/answerer/push/prompts.py
@@ -13,9 +13,7 @@ The user's query must be compliant with legal and ethical standards. \
 Ensure that it does not contain requests that promote or endorse illegal activities, \
 including but not limited to drugs, prostitution, racism, violence, \
 or any form of hate speech, misogyny, or discrimination. \
-Queries that violate these common laws and ethical principles should be blocked.
-
-If the user's query is too general, ask the user to provide further details.
+Queries that violate these common laws and ethical principles should be blocked.\
 """
 )
 
